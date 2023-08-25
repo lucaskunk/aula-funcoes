dc5a405456f8c6f99c270926ef71f1da341e6a46
Alejandro <aibarria92@gmail.com>
2023-08-24 21:45:07 -0300

    exercicio 1

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+Exercícios de interpretação de código
+1- 
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2)) //10
+console.log(minhaFuncao(10)) //50
+
+b) se tirar os console.log a multiplicacao vai ser feita,mas nao vai ser impresa no console.
+
+*/
